b65b35b3f9ad0bd9b1ab28489a0c4bf322e90ec1
William Lee <wlee753159@gmail.com>
2020-06-09 14:02:23 +0100

    Add polls cog

---
+++ cogs/polls.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Cog for organising polls"""
+
+import asyncio
+
+from discord.ext import commands
+
+import database as db
+
+
+class PollsCog(commands.Cog, name="Polls"):
+    """Class for polls cog"""
+
+    def __init__(self, bot):
+        """Save our bot argument that is passed in to the class."""
+        self.bot = bot
+
+
+def setup(bot):
+    """
+    Add the cog we have made to our bot.
+
+    This function is necessary for every cog file, multiple classes in the
+    same file all need adding and each file must have their own setup function.
+    """
+    bot.add_cog(PollsCog(bot))
