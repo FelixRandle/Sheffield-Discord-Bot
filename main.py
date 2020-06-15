0fec6ba2a0b7b4ade5eb9befbf0f2998f375a3f6
Felix <felix@randle.org>
2020-06-15 21:02:42 +0100

    Updated welcome message.

--- main.py
+++ main.py
@@ -58,15 +58,20 @@ async def on_member_join(member):
     """Send user a welcome message."""
     await member.create_dm()
     await member.dm_channel.send(
-        f'Hey {member.name}, welcome to the University of Sheffield Computer '
-        'Science Freshers Discord!\n'
+        f'Hey {member.name}, welcome to the (unofficial) University of Sheffield Computer '
+        'Science Discord!\n'
         'We like to know who we\'re talking to, so please change your '
         'nickname on the server to include your real name in some way.\n'
         'Apart from that, have fun on the server, get to know people and '
         'feel free to ask any questions about the course that you may have, '
         'we\'re all here to help each other!\n'
         'Many thanks,\n'
-        'The Discord Server Admin Team'
+        'The Discord Server Admin Team\n\n'
+        'As a note, all messages that you send on the server are logged.\n'
+        'This is to help us in the case of messages that contain'
+        'offensive content and need to be reported.\n'
+        'If you would like your logged messages to be'
+        'removed for any reason, please contact <@247428233086238720>.'
     )
 
     await db.add_user(member.id, member.bot)
