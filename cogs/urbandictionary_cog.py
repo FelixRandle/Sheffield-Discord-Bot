cc74730a161e470ce061bd38a85e5ebc5c4a6a6a
Ben Ridings <benridings@hotmail,com>
2020-07-01 19:53:07 +0100

    refactor and change command to ud

---
+++ cogs/urbandictionary_cog.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" 
+The Urban Dictionary cog written by Ben Ridings,
+You can randomly search a word or search a specific word
+"""
+import json
+
+import aiohttp
+import discord
+from discord.ext import commands
+
+# Constantы
+BASEURL = 'https://api.urbandictionary.com/v0/'
+
+
+class UrbanDictionaryCog(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Search a word the user types in
+    async def search_query(self, querystring):
+        async with aiohttp.ClientSession() as session:
+            data = await self.fetch(session, BASEURL + f'define?term={querystring}')
+            return data
+
+    # Fetch the URL
+    async def fetch(self, session, url):
+        async with session.get(url) as response:
+            return await response.text()
+
+    # Search up a random word
+    async def search_random_word(self):
+        async with aiohttp.ClientSession() as session:
+            data = await self.fetch(session, BASEURL + 'random')
+            return data
+
+    # Parse the searched word and display here
+    @commands.command(name='ud')
+    async def search_dictionary(self, ctx, *, query=None):
+        if query is None:
+            response_text = await self.search_random_word()
+        else:
+            response_text = await self.search_query(query)
+
+        definitions_list = json.loads(response_text)['list']
+        definition = definitions_list[0]
+
+        await ctx.send("Definition of {word}: {definition}".format(**definition))
+        await ctx.send("Example of {word}: {example}".format(**definition))
+
+
+def setup(bot):
+    bot.add_cog(UrbanDictionaryCog(bot))
