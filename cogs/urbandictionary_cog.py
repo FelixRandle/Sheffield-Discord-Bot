881de20c0356c9aeacc1ff7049f313f723edf3b1
Ben Ridings <benridings@hotmail,com>
2020-08-25 20:31:27 +0100

    Fix errors, create embed

--- cogs/urbandictionary_cog.py
+++ cogs/urbandictionary_cog.py
@@ -9,8 +9,10 @@ import json
 import aiohttp
 from discord.ext import commands
 
-# Constantы
+# Constant
 BASEURL = 'https://api.urbandictionary.com/v0/'
+NEXT_DEFINITION = '➡️'
+EMBED_COLOUR = 0xcf1e25
 
 
 class UrbanDictionaryCog(commands.Cog):
@@ -18,8 +20,46 @@ class UrbanDictionaryCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def create_embed(self,definitions, ctx):
+        def check_reaction(reaction, user):
+            return (str(reaction.emoji) == NEXT_DEFINITION
+            and reaction.message.id == message.id
+            and user.id != bot.user.id)
+
+        counter = 0
+        display = True
+        message = None
+
+        while display:
+
+            definition = definitions[counter]['definition']
+            example = definitions[counter]['example']
+            word = definitions[counter]['word']
+
+            embed = discord.Embed(title="Defining...", color=EMBED_COLOUR)
+
+            embed.add_field(name='Word', value= word)
+            embed.add_field(name="Definition", value= definition)
+            embed.add_field(name='Example', value= example)
+
+            if message is None:
+                message = await ctx.send(embed=embed)
+                # add emoji to message
+                await message.add_reaction(NEXT_DEFINITION)
+            else:
+                await message.edit(embed=embed)
+            
+            try:
+                reaction, user = await bot.wait_for(
+                    'reaction_add', check=check_reaction, timeout=60.0)
+                counter = (counter + 1) % len(definitions)
+            except asyncio.TimeoutError:
+                await message.delete()
+                break
+            await reaction.remove(user)
+
     # Search a word the user types in
-    async def search_query(self, querystring):
+    async def search_query(self, ctx,*, querystring):
         async with aiohttp.ClientSession() as session:
             data = await self.fetch(
                 session, BASEURL + f'define?term={querystring}')
@@ -40,17 +80,14 @@ class UrbanDictionaryCog(commands.Cog):
     @commands.command(name='ud')
     async def search_dictionary(self, ctx, *, query=None):
         if query is None:
-            response_text = await self.search_random_word()
+            definition_list = json.loads(await search_random_word())['list']
+
         else:
-            response_text = await self.search_query(query)
+            # Gets the typed in query and parses it
+            querystring = query
+            definition_list = json.loads(await search_query(querystring))['list']
 
-        definitions_list = json.loads(response_text)['list']
-        definition = definitions_list[0]
-
-        await ctx.send(
-            "Definition of {word}: {definition}".format(**definition))
-        await ctx.send("Example of {word}: {example}".format(**definition))
-
+        await create_embed(definition_list, ctx)
 
 def setup(bot):
     bot.add_cog(UrbanDictionaryCog(bot))
