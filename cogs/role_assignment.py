7cd6f9ec168d12efb9a38a4a1df6cb1480d6ad0b
William Lee <wlee753159@gmail.com>
2020-08-25 22:01:01 +0100

    Add role assignment cog with send message command

---
+++ cogs/role_assignment.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Cog for assigning roles to users
+"""
+
+from discord.ext import commands
+
+from models import Guild
+
+ROLE_ASSIGNMENT_MESSAGE = (
+    "Hi everyone!\n"
+    "To make it easier for everyone to see who's in which year, "
+    "and make sure you get stuff targeted for your year group,\n"
+    "please react below with your year to assign yourself your year's role.\n"
+    "If you misclick, just react again with the correct year to be reassigned"
+)
+
+
+class RoleAssignmentCog(commands.Cog, name="Role Assignment"):
+
+    def __init__(self, bot):
+        """
+        Instantiates an instance of the cog.
+        Takes the bot object as its single argument.
+        """
+        self.bot = bot
+
+    @commands.command(name="sendRoleAssignmentMsg")
+    @commands.has_permissions(manage_messages=True)
+    async def send_role_assignment_message(self, ctx):
+        """
+        Sends the role assignment message
+        """
+        guild = Guild.find(ctx.guild.id)
+        message = await ctx.send(ROLE_ASSIGNMENT_MESSAGE)
+        guild.role_assignment_msg_id = message.id
+        guild.save()
+
+
+def setup(bot):
+    """
+    Add the cog to the bot
+    """
+    bot.add_cog(RoleAssignmentCog(bot))
