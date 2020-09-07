6cf295d4ce1aa3dc72b28fbc54b9919c47198a89
William Lee <wlee753159@gmail.com>
2020-09-07 09:43:42 +0100

    Add RemoveReaction context manager

--- cogs/role_assignment.py
+++ cogs/role_assignment.py
@@ -7,6 +7,7 @@ Cog for assigning roles to users
 
 from discord.ext import commands
 
+import utils as ut
 from models import Guild
 
 EMOJI_TO_ROLES = {
@@ -61,39 +62,37 @@ class RoleAssignmentCog(commands.Cog, name="Role Assignment"):
         guild.role_assignment_msg_id = message.id
         guild.save()
 
-    @commands.Cog.listener()
+    @commands.Cog.listener('on_raw_reaction_add')
     async def on_raw_reaction_add(self, payload):
         # Member is bot, or the emoji is not relevant
         if payload.member.bot or str(payload.emoji) not in EMOJI_TO_ROLES:
             return
-        channel = await self.bot.fetch_channel(payload.channel_id)
-        # Get the corresponding role name for the emoji
-        role_name = EMOJI_TO_ROLES[str(payload.emoji)]
-        # Get member's existing roles
-        member_role_names = [str(role) for role in payload.member.roles]
-        if role_name in member_role_names:
-            return  # Return if the member is already assigned the role
-        # Fetch the available roles from the server
-        roles = await payload.member.guild.fetch_roles()
-        # Get roles that are mutually exclusive
-        mutex_roles = [role for role in roles if str(role) in MUTEX_ROLES]
-        # Role to be assigned
-        for role in roles:
-            if str(role) == role_name:
-                role_to_be_assigned = role
-                break
-        else:  # End of for loop means role was not found
-            return await channel.send(
-                f"Role {role_name!r} does not exist. "
-                "Please report this issue to an admin.")
-        # Removes existing mutually exclusive roles
-        if role_to_be_assigned in mutex_roles:
-            await payload.member.remove_roles(*mutex_roles)
-        # Adds the required role
-        await payload.member.add_roles(role_to_be_assigned)
-        # Remove the reaction from the message
-        msg = await channel.fetch_message(payload.message_id)
-        await msg.remove_reaction(payload.emoji, payload.member)
+        async with ut.RemoveReaction(self, payload):
+            # Get the corresponding role name for the emoji
+            role_name = EMOJI_TO_ROLES[str(payload.emoji)]
+            # Get member's existing roles
+            member_role_names = [str(role) for role in payload.member.roles]
+            if role_name in member_role_names:
+                return  # Return if the member is already assigned the role
+            # Fetch the available roles from the server
+            roles = await payload.member.guild.fetch_roles()
+            # Get roles that are mutually exclusive
+            mutex_roles = [role for role in roles if str(role) in MUTEX_ROLES]
+            # Role to be assigned
+            for role in roles:
+                if str(role) == role_name:
+                    role_to_be_assigned = role
+                    break
+            else:  # End of for loop means role was not found
+                channel = await self.bot.fetch_channel(payload.channel_id)
+                return await channel.send(
+                    f"Role {role_name!r} does not exist. "
+                    "Please report this issue to an admin.")
+            # Removes existing mutually exclusive roles
+            if role_to_be_assigned in mutex_roles:
+                await payload.member.remove_roles(*mutex_roles)
+            # Adds the required role
+            await payload.member.add_roles(role_to_be_assigned)
 
 
 def setup(bot):
