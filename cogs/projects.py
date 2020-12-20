380b90ad212241b2ddb92708d8f3709b9b05335c
William Lee <wlee753159@gmail.com>
2020-12-20 21:03:48 +0000

    Add link regex validation

--- cogs/projects.py
+++ cogs/projects.py
@@ -5,7 +5,13 @@
 A cog for showcasing GitHub project repos
 """
 
+import re
+
 from discord.ext import commands
+
+
+GITHUB_LINK_REGEX = re.compile(r"github.com/(?P<owner>\S+)/(?P<repo>\S+)")
+API_TEMPLATE = "https://api.github.com/repos/{owner}/{repo}"
 
 
 class ProjectsCog(commands.Cog, name="Projects"):
@@ -16,13 +22,14 @@ class ProjectsCog(commands.Cog, name="Projects"):
 
     @commands.command(help="Showcase your projects on GitHub")
     @commands.has_role("Member")
-    async def projects(self, ctx):
+    async def project(self, ctx, *, repo_link: str):
         """
         Allows users to showcase projects from GitHub using GitHub repo links
 
         Extracts info from the repo and displays it in an embed
         """
-        pass
+        match = GITHUB_LINK_REGEX.search(repo_link)
+        await ctx.send("Link valid" if match else "Link invalid")
 
 
 def setup(bot):
